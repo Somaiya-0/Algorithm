04e3d22e621ab818c3ead68d72356dfe4293fa2c
Somaiya-0 <sdark.babe@gmail.com>
2024-09-15 10:54:33 +0600

    comment updated

--- algo_c++.cpp/c++_1st_program.cpp
+++ algo_c++.cpp/c++_1st_program.cpp
@@ -1,59 +1,50 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main ()
-{
+
+int main() {
+    int size, sum = 0, even_sum = 0, odd_sum = 0;
+    vector<int> n;  // Vector to store input integers
     
-    // n.push_back(6);
-    // char s[] = "Hello World!";
-    // int money;
-    // cin>>money;
-    // if (money<50)
-    // {
-    //     for (int i : n) 
-    //     {
-    //     cout << i << "  ";
-    //     }
-    //     cout<<endl;
-    //     n.pop_back();
-    //     for (int i : n) 
-    //     {
-    //     cout << i << "  ";
-    //     }
-    //     cout<<"dukkho dukkho sob dukkho"<<" "<<endl<<s;
-    // }
+    // Input the number of elements
+    cin >> size;
 
-    int size,sum=0,even_sum=0,odd_sum=0;
-    vector<int> n={};
-    cin>>size;
-    for (int i=0;i<=size;i++) 
-        {
-        cin>> i ;
-        n.push_back(i);
-
-        }
-    for (int i : n) 
-       {
-        cout << i << "  ";
-        }
-    for (int i =0;i<size;i++) 
-        {  
-            sum+=n[i];
-            if(i%2==0){
-                even_sum+=n[i];
-            }
-            else{
-                odd_sum+=n[i];
-            }
-
-          
-        }
-        cout<<endl<<"Sum of elements : "<<sum<<endl<<"Sum of even index elements: "<<even_sum<<endl<<"Sum of odd index elements : "<<odd_sum;
-        if(even_sum<odd_sum){
-            cout<<endl<<"Sum of even index elements is bigger";
-        }
-        else{
-            cout<<endl<<"Sum of odd index elements is bigger";
-        }
-
+    // Input elements into the vector
+    for (int i = 0; i < size; i++) {
+        int num;
+        cin >> num;
+        n.push_back(num);
     }
+
+    // Print all elements of the vector
+    for (int i : n) {
+        cout << i << "  ";
+    }
+    cout << endl;
+
+    // Calculate sums and categorize based on index parity
+    for (int i = 0; i < size; i++) {
+        sum += n[i];
+        if (i % 2 == 0) {
+            // Even index
+            even_sum += n[i];
+        } else {
+            // Odd index
+            odd_sum += n[i];
+        }
+    }
+
+    // Output the results
+    cout << "Sum of elements: " << sum << endl;
+    cout << "Sum of even index elements: " << even_sum << endl;
+    cout << "Sum of odd index elements: " << odd_sum << endl;
+
+    // Compare sums and print result
+    if (even_sum < odd_sum) {
+        cout << "Sum of even index elements is smaller" << endl;
+    } else {
+        cout << "Sum of odd index elements is smaller" << endl;
+    }
+
+    return 0;
+}
