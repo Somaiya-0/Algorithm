f65a34cbf8c11b069923d2971958553930e26d60
Somaiya-0 <sdark.babe@gmail.com>
2024-09-15 11:24:21 +0600

    commented

--- algo_python.py/Bubble_Sort.py
+++ algo_python.py/Bubble_Sort.py
@@ -1,18 +1,19 @@
-a=[]
-n=int(input("Enter range :" ,))
-for j in range(0,n):
-    b=int(input("",))
-    a.append(b)
-print("before sorting : ",a)
+a = []
+n = int(input("Enter range: "))
 
-print("Processing : ")
-for i in range(0,n-1):
-    for j in range(0,n-i-1):
-        if a[j]>a[j+1]:
-            temp=a[j]
-            a[j]=a[j+1]
-            a[j+1]=temp
-            print(a)
-            print(i)
-    print(a)   
-print("Sorted : ",a)
+user_input = input(f"Enter {n} numbers separated by spaces: ")
+for num in user_input.split():
+    a.append(int(num))
+
+print("Before sorting: ", a)
+
+# Bubble Sort Algoritm
+for i in range(0, n - 1):
+    for j in range(0, n - i - 1):
+        # Swap 
+        if a[j] > a[j + 1]:
+            temp = a[j]
+            a[j] = a[j + 1]
+            a[j + 1] = temp
+
+print("Sorted: ", a)
